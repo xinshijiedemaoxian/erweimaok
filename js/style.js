523e72fff0620fa6b330f1290b12be5931105d1e
xinshijiedemaoxian <xia_duanjian!163.com>
2016-11-09 10:44:23 +0800

    focus

--- js/style.js
+++ js/style.js
@@ -1,6 +1,9 @@
 var qrcode = new QRCode(document.getElementById("qrcode"), {});
 		$(function(){
-			browserRedirect()
+			$("#desc").focus(function(){
+            $("#desc").val("");
+        });
+			browserRedirect();
 		});
 		function browserRedirect() {
 		        if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))){
