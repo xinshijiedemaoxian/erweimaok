36ac42f7cfdaaacbf7f9ba72ce0c3e5af0ac3c81
xinshijiedemaoxian <xia_duanjian!163.com>
2016-11-09 10:38:14 +0800

    yidong

--- js/style.js
+++ js/style.js
@@ -5,17 +5,17 @@ var qrcode = new QRCode(document.getElementById("qrcode"), {});
 		function browserRedirect() {
 		        if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))){
 		             $(".button").on('touchstart',function(e){
-		                $(".button")[0].style.border="5px solid orange";
+		                hechen();
 		                event.preventDefault()
 		            }).on('touchend ',function(e){
-		                $(".button")[0].style.color="green";
+		                makeCode();
 		                event.preventDefault()
 		            });
 		        } else {
 		            $(".button").on('mousedown',function(e){
-		       			makeCode();
+		       			hechen();
 		            }).on('mouseup ',function(e){
-		                 hechen();
+		                 makeCode();
 		            });
 		        }
 		    };
